115f2f17bbeb0478201a5460d42413acd68a4701
Hrithik Gupta <93973566+hrx0hrithik@users.noreply.github.com>
2023-07-22 03:27:23 +0530

    Update NoteState.js

--- frontend/src/context/notes/NoteState.js
+++ frontend/src/context/notes/NoteState.js
@@ -2,7 +2,7 @@ import NoteContext from "./noteContext";
 import { useState } from "react";
 
 const NoteState = (props) => {
-  const host = "https://notes-app-4p6a.onrender.com";
+  const host = "https://note-app-backend-zb7r.onrender.com";
   const notesInitial = [];
   const [notes, setNotes] = useState(notesInitial);
 
