bddff23a04e4d27f02d5c1d1f1cd3686542a7e3c
hrx0hrithik <hrithikgupta52@gmail.com>
2023-07-22 12:40:18 +0530

    mongodb url

--- backend/db.js
+++ backend/db.js
@@ -5,7 +5,10 @@ const mongoUrl = process.env.MONGO_URL
 
 const connectToMongo = async () => {
   try {
-    await mongoose.connect(mongoUrl);
+    await mongoose.connect(mongoUrl,{
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
     console.log("Connected to MongoDB");
   } catch (error) {
     console.log(error);
